14173e667d04aedbf969ff7d361ff12468b90dae
deedat5 <deedat5@yahoo.com>
2020-03-07 15:42:35 +0000

    passed cors middleware to fetchallvoters route

--- src/router/user.js
+++ src/router/user.js
@@ -67,7 +67,7 @@ router.post('/users/me/logoutall', auth, async(req, res) => {
     }
 })
 var whitelist = ['https://ghana-market-association.firebaseapp.com']
-export var corsOptionsDelegate = function (req, callback) {
+var corsOptionsDelegate = function (req, callback) {
     var corsOptions;
     if (whitelist.indexOf(req.header('Origin')) !== -1) {
       corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
