4ff4dfb8113953636a8b4bde9d4f7f68289175d7
deedat5 <deedat5@yahoo.com>
2020-04-03 16:10:19 +0000

    cors

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
- const url="mongodb+srv://deedat5:Portable24198@cluster0-tkvnj.mongodb.net/voters?retryWrites=true&w=majority" 
+ const url="mongodb+srv://deedat5:Portable24198@cluster0-tkvnj.mongodb.net/Election?retryWrites=true&w=majority" 
 mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
